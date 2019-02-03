19097e5459f144c9da7d2177566ebcf41c055713
Goric <marcin.m.jedrasik@gmail.com>
2019-02-03 15:01:53 +0100

    Addded RenderingOptions to the commons.

---
+++ BenoitCommons/RenderingOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BenoitCommons
+{
+    public class RenderingOptions
+    {
+        /// <summary>
+        /// Width in pixels of the render frame.
+        /// </summary>
+        public int FrameWidth { get; set; }
+
+        /// <summary>
+        /// Height in pixels of the render frame.
+        /// </summary>
+        public int FrameHeight { get; set; }
+
+        /// <summary>
+        /// Maximum iteration for the escape time algorithm.
+        /// </summary>
+        public int MaxIteration { get; set; }
+
+        /// <summary>
+        /// The absolute value that specifies the divergence threshold.
+        /// </summary>
+        public int BailoutValue { get; set; }
+    }
+}
