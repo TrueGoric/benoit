8058aadb7c5f598ca69675de3e6dad6fad2ac0ba
Goric <marcin.m.jedrasik@gmail.com>
2019-02-03 17:39:31 +0100

    Added default option values.

--- BenoitCommons/RenderingOptions.cs
+++ BenoitCommons/RenderingOptions.cs
@@ -7,21 +7,21 @@ namespace BenoitCommons
         /// <summary>
         /// Width in pixels of the render frame.
         /// </summary>
-        public int FrameWidth { get; set; }
+        public int FrameWidth { get; set; } = 350;
 
         /// <summary>
         /// Height in pixels of the render frame.
         /// </summary>
-        public int FrameHeight { get; set; }
+        public int FrameHeight { get; set; } = 200;
 
         /// <summary>
         /// Maximum iteration for the escape time algorithm.
         /// </summary>
-        public int MaxIteration { get; set; }
+        public int MaxIteration { get; set; } = 1000;
 
         /// <summary>
-        /// The absolute value that specifies the divergence threshold.
+        /// The absolute value that specifies the divergence threshold from the magnitude.
         /// </summary>
-        public int BailoutValue { get; set; }
+        public int BailoutValue { get; set; } = 1 << 8;
     }
 }
