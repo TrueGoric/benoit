0a3d00786a4b7edda4cc603e882e261ea231fa3d
Goric <marcin.m.jedrasik@gmail.com>
2019-02-03 22:06:10 +0100

    Implemented BenoitSilo.

--- BenoitSilo/Program.cs
+++ BenoitSilo/Program.cs
@@ -1,13 +1,49 @@
 ﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using BenoitGrains;
 
 namespace BenoitSilo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("Starting Benoit silo...");
+            var host = await StartSilo();
+            Console.WriteLine("Benoit silo is running.");
+            
+            Console.WriteLine("Press Enter to terminate...");
+            Console.ReadLine();
+
+            await host.StopAsync();
+        }
+
+        static async Task<ISiloHost> StartSilo()
+        {
+            // https://dotnet.github.io/orleans/Documentation/clusters_and_clients/configuration_guide/local_development_configuration.html
+
+            var builder = new SiloHostBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(o =>
+                {
+                    o.ClusterId = "dev";
+                    o.ServiceId = "Benoit";
+                })
+                .Configure<EndpointOptions>(o => o.AdvertisedIPAddress = IPAddress.Loopback)
+                .ConfigureLogging(l => l.AddConsole())
+                .ConfigureApplicationParts(parts => parts
+                    .AddApplicationPart(typeof(RenderingDispatcher<int>).Assembly).WithReferences());
+                
+            var silo = builder.Build();
+            
+            await silo.StartAsync();
+            return silo;
         }
     }
 }
